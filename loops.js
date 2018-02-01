30f5987f353e5fb1e8f80e8d62f6baf650a56e4e
Devin DiNapoli <devindinapoli@gmail.com>
2018-02-01 20:22:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
-  for (let i = 0; i<25; i++)
+  for (let i = 0; i<25; i++) {
+    if (i === 1) {
+      
+    }
+  }
     
     else  (i<25; i++){
       console.log(`I am ${i} strange loop`)
